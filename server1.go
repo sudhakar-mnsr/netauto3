48db9db55e78d4f3427caaf0e4d88d44cc21f2c2
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-17 23:58:15 +0530

    added more

--- server1.go
+++ server1.go
@@ -12,4 +12,7 @@ func main() {
    log.Fatal(http.ListenAndServe(localhost:8000", nil))
 }
 
-
+// handler echoes the path component of the requested URL.
+func handler(w http.ResponseWriter, r *http.Request) {
+   fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
