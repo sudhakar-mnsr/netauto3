6e65e36f99febd1c697ec3475e6918cae89d2f2d
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-18 01:38:13 +0530

    changed

--- fetch2.go
+++ fetch2.go
@@ -22,3 +22,19 @@ func main() {
    fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
 }
 
+fun fetch(url string, ch chan<- string) {
+   start := time.Now()
+   resp, err := http.Get(url)
+   if err != nil {
+      ch <- fmt.Sprint(err) // send to channel ch
+      return 
+   }
+   nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+   resp.Body.Close() // dont leak resources
+   if err != nil {
+      ch <- fmt.Sprintf("while reading %s: %v", url, err)
+      return
+   }
+   secs := time.Since(start).Seconds()
+   ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
