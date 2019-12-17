9ed95db2814f325aa0a9ffd5a410f0ef95ce803c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-17 23:48:42 +0530

    changed dup3

--- dup3.go
+++ dup3.go
@@ -9,4 +9,12 @@ import (
 
 func main() {
 counts := make(map[string]int)
-for _, filename := range os.Args
+for _, filename := range os.Args[1:] {
+   data, err := ioutil.ReadFile(filename)
+   if err != nil {
+      fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+      continue
+   }
+   for _, line := range strings.Split(string(data), "\n") {
+      counts[line]++
+   }
