dcc81135bef5e51b9656a5417dd5fa5e19c83b49
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-18 00:00:28 +0530

    minor typo

--- server1.go
+++ server1.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
    http.HandleFunc("/", handler) // each request calls handler
-   log.Fatal(http.ListenAndServe(localhost:8000", nil))
+   log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 // handler echoes the path component of the requested URL.
