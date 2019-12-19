3b03050d61d62d62a40182568816bf7b4c0467af
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-19 14:27:59 +0530

    added benchmarks

--- popcount_test.go
+++ popcount_test.go
@@ -33,3 +33,28 @@ func PopCountByShifting(x uint64) int {
    }
    return n
 } 
+
+// Benchmarks
+func BenchmarkPopCount(b *testing.B) {
+   for i := 0; i < b.N; i++ {
+      popcount.PopCount(0x1234567890ABCDEF)
+   }
+}  
+
+func BenchmarkBitCount(b *testing.B) {
+   for i := 0; i < b.N; i++ {
+      BitCount(0x1234567890ABCDEF)
+   }
+}
+
+func BenchmarkPopCountByClearing(b *testing.B) {
+   for i := 0; i < b.N; i++ {
+      PopCountByClearing(0x1234567890ABCDEF)
+   }
+}
+
+func BenchmarkPopCountByShifting(b *testing.B) {
+   for i := 0; i < b.N; i++ {
+      PopCountByShifting(0x1234567890ABCDEF)
+   }
+}
