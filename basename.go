d3d49cc796b580c34e434fb28f5b81b467106f71
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-21 17:58:16 +0530

    basename

---
+++ basename.go
@@ -0,0 +1,31 @@
+// basename removes directory components and a .suffix
+// e.g., a => a, a.go => a, a/b/c.go => c, a/b.c.go => b.c
+package main
+
+import (
+   "fmt"
+   "os"
+)
+
+func basename(s string) string {
+   // Discard last '/' and everything before.
+   for i := len(s) - 1; i >= 0; i-- {
+      if s[i] == '/' {
+         s = s[i+1:]
+         break
+      }
+   }
+   
+   // Preserve everything before last .
+   for i := len(s) - 1; i >= 0; i -- {
+      if s[i] == '.' {
+         s = s[:i]
+         break
+      }
+   }
+   return s
+}
+
+func main() {
+   fmt.Println(basename(os.Args[1]))
+}
