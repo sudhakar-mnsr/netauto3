c250ad64d822e2a1e54d04cf77171e0377b79bf3
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-22 11:13:28 +0530

    array

---
+++ array.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+   var a [3]int
+   fmt.Println(a[0])
+   fmt.Println(a[len(a)-1])
+   
+   for i, v := range a {
+      fmt.Printf("%d %d\n", i, v)
+   }
+   
+   // print elements only
+   for _, v := range a {
+      fmt.Printf("%d\n", v)
+   }
+}
