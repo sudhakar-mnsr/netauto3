05efe5e7117fd750eb5f0c81291d68a2396e3f69
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-24 13:47:36 +0530

    folder changes

--- func2.go
+++ func2.go
@@ -3,8 +3,22 @@ package main
 import (
    "fmt"
    "os"
-   "html/html"
+   "html"
 )
+
+func visit(links []string, n *html.Node) []string {
+   if n.Type == html.ElementNode && n.Data == "a" {
+      for _, a := range n.Attr {
+         if a.Key == "href" {
+            links = append(links, a.Val)
+         }
+      }
+   }
+   for c := n.FirstChild; c != nil; c = c.NextSibling {
+      links = visit(links, c)
+   }
+   return links
+}
 
 func main() {
    doc, err := html.Parse(os.Stdin)
