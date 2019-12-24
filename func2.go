e2d50df49325c70f4fba1e07f27ed765eebae939
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-24 13:38:28 +0530

    func2

---
+++ func2.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+   "fmt"
+   "os"
+   "html/html"
+)
+
+func main() {
+   doc, err := html.Parse(os.Stdin)
+   if err != nil {
+      fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+      os.Exit(1)
+   }
+   for _, link := range visit(nil, doc) {
+      fmt.Println(link)
+   }
+}
