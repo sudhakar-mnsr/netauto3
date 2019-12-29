509f3e9264942c645ffb01acb7728ff1aaf90b10
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-30 00:40:27 +0530

    typo

--- src/geometry/geometry.go
+++ src/geometry/geometry.go
@@ -7,7 +7,7 @@ import "math"
 type Point struct{ X, Y float64 }
 
 // traditional function 
-func Distance(p, q, Point) float64 {
+func Distance(p, q Point) float64 {
    return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
