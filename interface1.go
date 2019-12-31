d1c847af5f050cb33b38a7bef2833180b7a7963f
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2019-12-31 13:47:07 +0530

    added custom sort function

--- interface1.go
+++ interface1.go
@@ -56,4 +56,47 @@ func (x byYear) Len() int { return len(x) }
 func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
 func (x byYear) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
 
+func main() {
+   fmt.Println("byArtist:")
+   sort.Sort(byArtist(tracks))
+   printTracks(tracks)
+   
+   fmt.Println("\nReverse(byArtist):")
+   sort.Sort(sort.Reverse(byArtist(tracks)))
+   printTracks(tracks)
+   
+   fmt.Println("\nCustom:")
+   sort.Sort(customSort{tracks, func(x, y *Track) bool {
+   if x.Title != y.Title {
+      return x.Title < y.Title
+   }
+   if x.Year != y.Year {
+      return x.Year < y.Year
+   }
+   if x.Length != y.Length {
+      return x.Length < y.Length
+   }
+   return false
+   }})
+   printTracks(tracks)
+}
 
+type customSort struct {
+   t []*Track
+   less func(x, y *Track) bool
+}
+
+func (x customSort) Len() int { return len(x.t) }
+func (x customSort) Less(i, j int) bool { return x.less(x.t[i], x.t[j]) }
+func (x customSort) Swap(i, j int) { x.t[i], x.t[j] = x.t[j], x.t[i] }
+
+func init() {
+   values := []int{3,1,4,1}
+   fmt.Println(sort.IntsAreSorted(values))
+   sort.Ints(values)
+   fmt.Println(values)
+   fmt.Println(sort.IntsAreSorted(values))
+   sort.Sort(sort.Reverse(sort.IntSlice(values)))
+   fmt.Println(values)
+   fmt.Println(sort.IntsAreSorted(values))
+}
