b98110079379ffface2c3bf64317ac92f5ff0f9b
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-11 23:28:47 +0530

    composition added for plane

--- constructor.go
+++ constructor.go
@@ -56,3 +56,25 @@ func newPlane(mk, mdl string) *plane {
 	return p
 }
 
+func (p *plane) fly() {
+	fmt.Printf("Aircraft %s %s clear for takeoff!\n", p.make, p.model)
+}
+
+func main() {
+	t := newTruck("Ford", "F750")
+	t.axels = 2
+	t.wheels = 6
+	t.class = 3
+	t.start()
+	t.drive()
+
+	p := newPlane("HondaJet", "HA-420")
+	p.fuel = JET
+	p.thrust = 2050
+	p.engineCount = 2
+	p.fixedWings = true
+	p.maxAltitude = 43000
+	p.start()
+	p.fly()
+
+}
