9ee6309ecc30e14371c0d637e06a3e24a0085df4
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-12 02:50:25 +0530

    interface9.go

---
+++ interface9.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+   var anyType interface{}
+   anyType = 77.0
+   anyType = "Iam string now"
+   fmt.Println(anyType)
+   
+   printAnyType("The car is slow")
+   m := map[string]string{"ID": "12345", "name": "Kerry"}
+   
+   printAnyType(m)
+   printAnyType(1253443455)
+}
+func printAnyType(val interface{}) {  
+   fmt.Println(val)
+}
