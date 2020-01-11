238b7aeee42d0aaac960145f0574484e37c90be4
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-12 02:34:34 +0530

    composition added for plane

--- interface7.go
+++ interface7.go
@@ -50,3 +50,37 @@ func (t *triangle) String() string {
    return fmt.Sprintf("%s[sides: a=%.2f b=%.2f c=%.2f]", t.name, t.a, t.b, t.c,)
 }
 
+type circle struct {
+	name string
+	rad  float64
+}
+
+func (c *circle) area() float64 {
+	return math.Pi * (c.rad * c.rad)
+}
+
+func (c *circle) circonf() float64 {
+	return 2 * math.Pi * c.rad
+}
+
+func (c *circle) String() string {
+	return fmt.Sprintf(
+		"%s[rad: a=%.2f diam=%.2f]",
+		c.name, c.rad, (2 * c.rad),
+	)
+}
+
+func shapeInfo(s shape) string {
+	return fmt.Sprintf("Area = %.2f", s.area())
+}
+
+func main() {
+	r := &rect{"Square", 4.0, 4.0}
+	fmt.Println(r, "=>", shapeInfo(r))
+
+	t := &triangle{"Right Triangle", 1, 2, 3}
+	fmt.Println(t, "=>", shapeInfo(t))
+
+	c := &circle{"Small circle", 20}
+	fmt.Println(c, "=>", shapeInfo(c))
+}
