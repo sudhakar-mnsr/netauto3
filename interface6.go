df28211e088b483df3b96d279bac488a15ed95b7
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-01-12 02:02:01 +0530

    composition added for plane

--- interface6.go
+++ interface6.go
@@ -27,4 +27,23 @@ func (r *rect) String() string {
    return fmt.Sprintf("%s[length:%.2f height:%.2f]", r.name, r.length, r.height,)
 }
 
+type triangle struct {
+   name string
+   a,b,c float64
+}
+
+func (t *triangle) area() float64 {
+   return 0.5 * (t.a * t.b)
+}
+func (t *triangle) perim() float64 {
+   return t.a + t.b + math.Sqrt((t.a*t.a)+(t.b*t.b))
+}
+
+func (t *triangle) String() string {
+   return fmt.Sprintf("%s[sides: a=%.2f b=%.2f c=%.2f]", t.name, t.a, t.b,t.c,)
+}
+
+func shapeInfo(s shape) string {
+   return fmt.Sprintf("Area = %.2f, Perim = %.2f, s.area(), s.perim())
+}
 
