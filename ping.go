50d350746919f0815af4bf3cf342188d640b38a7
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-14 23:55:04 +0530

    examples

--- ping.go
+++ ping.go
@@ -11,3 +11,23 @@ import (
 // change this to my own IP address or set to 0.0.0.0
 const myIPAddress = "192.168.1.2"
 const ipv4HeaderSize = 20
+
+func main() {
+   if len(os.Args) != 2 {
+      fmt.Println("Usage: ", os.Args[0], "host")
+      os.Exit(1)
+   }
+   localAddr, err := net.ResolveIPAddr("ip4", myIPAddress)
+   if err != nil {
+      fmt.Println("Resolution error", err.Error())
+      os.Exit(1)
+   }
+
+   remoteAddr, err := net.ResolveIPAddr("ip4", os.Args[1])
+   if err != nil {
+      fmt.Println("Resolution error", err.Error())
+      os.Exit(1)
+   }
+
+   conn, err := net.DialIP("ip4:icmp", localAddr, remoteAddr)
+   checkError(err)
