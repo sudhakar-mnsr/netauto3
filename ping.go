5bfe5e3bd5ba93eb8fd91bd0539367884f10c276
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-14 23:58:18 +0530

    examples

--- ping.go
+++ ping.go
@@ -32,28 +32,28 @@ func main() {
    conn, err := net.DialIP("ip4:icmp", localAddr, remoteAddr)
    checkError(err)
 
-        var msg [512]byte
-        msg[0] = 8  // echo
-        msg[1] = 0  // code 0
-        msg[2] = 0  // checksum, fix later
-        msg[3] = 0  // checksum, fix later
-        msg[4] = 0  // identifier[0]
-        msg[5] = 13 // identifier[1] (arbitrary)
-        msg[6] = 0  // sequence[0]
-        msg[7] = 37 // sequence[1] (arbitrary)
-        len := 8
+   var msg [512]byte
+   msg[0] = 8  // echo
+   msg[1] = 0  // code 0
+   msg[2] = 0  // checksum, fix later
+   msg[3] = 0  // checksum, fix later
+   msg[4] = 0  // identifier[0]
+   msg[5] = 13 // identifier[1] (arbitrary)
+   msg[6] = 0  // sequence[0]
+   msg[7] = 37 // sequence[1] (arbitrary)
+   len := 8
 
    // now fix checksum bytes
    check := checkSum(msg[0:len])
    msg[2] = byte(check >> 8)
    msg[3] = byte(check & 255)
 
-        // send the message
-        _, err = conn.Write(msg[0:len])
-        checkError(err)
+   // send the message
+   _, err = conn.Write(msg[0:len])
+   checkError(err)
 
-        fmt.Print("Message sent:    ")
-        for n := 0; n < 8; n++ {
-                fmt.Print(" ", msg[n])
-        }
-        fmt.Println()
+   fmt.Print("Message sent:    ")
+   for n := 0; n < 8; n++ {
+           fmt.Print(" ", msg[n])
+   }
+   fmt.Println()
