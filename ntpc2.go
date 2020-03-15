8895b23e50f53600949408a71552e787b9545b3c
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-15 23:40:14 +0530

    examples

--- ntpc2.go
+++ ntpc2.go
@@ -53,3 +53,13 @@ func main() {
       }
    }()
    
+   fmt.Printf("time from (unixgram) (%s)\n", conn.RemoteAddr())
+
+   // Once connection is established, the code pattern
+   // is the same as in the other impl.
+
+   // send time request
+   if _, err = conn.Write(req); err != nil {
+      fmt.Printf("failed to send request: %v\n", err)
+      os.Exit(1)
+   }
