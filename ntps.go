907925c8b05ea4772fbfac65339461ddc2260965
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-15 21:22:26 +0530

    examples

--- ntps.go
+++ ntps.go
@@ -28,3 +28,23 @@ func main() {
       os.Exit(1)
    }
    defer conn.Close()
+
+   fmt.Printf("listening for time requests: (udp) %s\n", conn.LocalAddr())
+
+   // From this point, the remainder of the code simply
+   // reads the incoming request and send a response
+   // read incoming requests.
+   // since we are using a sessionless proto, each request can 
+   // potentially go to a different client. Therefore, the ReadFormXXX
+   // operation returns the remote address (saved in raddr)
+   // where to send the response.
+   _, raddr, err := conn.ReadFromUDP(make([]byte, 48))
+   if err != nil {
+      fmt.Println("error getting request:", err)
+      os.Exit(1)
+   }
+   // ensure laddr is set
+   if raddr == nil {
+      fmt.Println("request missing remote addr")
+      os.Exit(1)
+   }
