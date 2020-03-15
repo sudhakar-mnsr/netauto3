07fc9e607cfdea833c6c550124fa857280d8bd26
Sudhakar MNSR <sudhakar.mnsr@gmail.com>
2020-03-15 23:35:27 +0530

    examples

--- ntpc2.go
+++ ntpc2.go
@@ -25,4 +25,20 @@ func main() {
    // a request setting for time server.
    req[0] = 0x1B
 
+   // rsp byte slice used to recieve server response
+   rsp := make([]byte, 48)
    
+   // create a remote address bound to the server socket
+   raddr, err := net.ResolveUnixAddr("unixgram", path)
+   if err != nil {
+      fmt.Println(err)
+      os.Exit(1)
+   }
+
+   // IMP: create a separate local address that is bound 
+   // to a different socket for the client. This is not done
+   // automatically when using unix socket datagram. Here, the 
+   // local address is given by <remote_socket_path>-client
+   laddr := &net.UnixAddr{Name: fmt.Sprintf("%s-client", raddr.Name), Net: "unixgram"} 
+
+   // setup a connection (net.UnixConn) using net.DialUnix
